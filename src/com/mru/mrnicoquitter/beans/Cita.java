6cca98150023ed0880551edf0c2af3fe9311f654
Mar Cial R <marcialemilio@gmail.com>
2010-06-11 03:36:00 +0200

    Nuevo Generador de Citas INSERTS

---
+++ src/com/mru/mrnicoquitter/beans/Cita.java
@@ -0,0 +1,27 @@
+package com.mru.mrnicoquitter.beans;
+
+public class Cita {
+	private int id;
+	private String text;
+	private String author;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+
+}
--- src/inserts/Puente.java
+++ src/inserts/Puente.java
@@ -12,57 +12,69 @@ import java.io.IOException;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
+
 import static com.mru.mrnicoquitter.Global.*;
 import com.google.gson.Gson;
+import com.mru.mrnicoquitter.beans.Cita;
 import com.mru.mrnicoquitter.beans.Stage;
 import com.mru.mrnicoquitter.db.flow.generator.FlowXMLParser;
+import com.mru.mrnicoquitter.utils.file.windows.FileUtils;
 
 public class Puente {
 
-	private static final String INPUT_FILE = "C:\\Users\\BEEP\\Desktop\\ANDROIDING\\eclipse\\workspace\\MrSmkQuitter\\assets\\HELPER_THINGS\\FlowObjects.xml";
-	private static final String OUTPUT_FILE_GSON = "C:\\Users\\BEEP\\Desktop\\ANDROIDING\\eclipse\\workspace\\MrSmkQuitter\\assets\\HELPER_THINGS\\globals.txt";
-	private static final String FLOW_INSERTS = "C:\\Users\\BEEP\\Desktop\\ANDROIDING\\eclipse\\workspace\\MrSmkQuitter\\res\\raw\\flow_inserts";
-	private static final String DAY_INSERTS = "C:\\Users\\BEEP\\Desktop\\ANDROIDING\\eclipse\\workspace\\MrSmkQuitter\\res\\raw\\day_inserts";	
+	private static final String INPUT_FLOW_OBJECTS_FILE 	= "C:\\Users\\BEEP\\Desktop\\ANDROIDING\\eclipse\\workspace\\MrSmkQuitter\\assets\\HELPER_THINGS\\FlowObjects.xml";
+	private static final String OUTPUT_FLOW_OBJECTS_INSERTS = "C:\\Users\\BEEP\\Desktop\\ANDROIDING\\eclipse\\workspace\\MrSmkQuitter\\res\\raw\\flow_inserts";
+	private static final String OUTPUT_FILE_GSON 			= "C:\\Users\\BEEP\\Desktop\\ANDROIDING\\eclipse\\workspace\\MrSmkQuitter\\assets\\HELPER_THINGS\\globals.txt";
+	private static final String INPUT_FILE_CITAS 			= "C:\\Users\\BEEP\\Desktop\\ANDROIDING\\eclipse\\workspace\\MrSmkQuitter\\assets\\HELPER_THINGS\\citas_plain.txt";
+	private static final String OUTPUT_FILE_CITAS 			= "C:\\Users\\BEEP\\Desktop\\ANDROIDING\\eclipse\\workspace\\MrSmkQuitter\\assets\\HELPER_THINGS\\citas_inserts.txt";	
+	private static final String OUTPUT_DAY_INSERTS 			= "C:\\Users\\BEEP\\Desktop\\ANDROIDING\\eclipse\\workspace\\MrSmkQuitter\\res\\raw\\day_inserts";	
 	private static StringBuilder sb;
 	private static Gson gson;
 	private static ObjectMapper mapper;
-	private static boolean json_not_jackson = false;
-	private static List<Stage> listita;
+	private static boolean json_not_jackson 				= false;
+	private static List<Stage> flowObjectsList;
+	private static List<Cita> citasObjectsList;
 	private static List<String> asJsonListita;
 	public static void main() {
-		File inputFile 			= new File(INPUT_FILE);
-		File gsonPlain 			= new File(OUTPUT_FILE_GSON);
-		File flowINSERTS		= new File(FLOW_INSERTS);
-		File dayINSERTS			= new File(DAY_INSERTS);		
-		gson 					= new Gson();
-		FlowXMLParser parser 	= new FlowXMLParser("");
-		mapper = new ObjectMapper(); // can reuse, share globally
-		listita = parser.parse(inputFile);
-		asJsonListita = new ArrayList<String> ();
-		asJsonListita = getGson(listita);
+		File f_INPUT_FLOW_OBJECTS_FILE 		= new File(INPUT_FLOW_OBJECTS_FILE);
+		File f_OUTPUT_FILE_GSON 			= new File(OUTPUT_FILE_GSON);
+		File f_OUTPUT_FLOW_OBJECTS_INSERTS	= new File(OUTPUT_FLOW_OBJECTS_INSERTS);
+		File f_OUTPUT_DAY_INSERTS			= new File(OUTPUT_DAY_INSERTS);
+		File f_INPUT_FILE_CITAS				= new File(INPUT_FILE_CITAS);
+		File f_OUTPUT_FILE_CITAS			= new File(OUTPUT_FILE_CITAS);		
+		gson 								= new Gson();
+		FlowXMLParser parser 				= new FlowXMLParser("");
+		mapper 								= new ObjectMapper(); // can reuse, share globally
+		citasObjectsList					= parseCitasInputFile(f_INPUT_FILE_CITAS);
+		flowObjectsList 					= parser.parse(f_INPUT_FLOW_OBJECTS_FILE);
+		asJsonListita 						= getGson(flowObjectsList);
 
 		// for (Entry<Object, Object> entry : System.getProperties().entrySet())
 		// {
 		// System.out.println(entry.getKey() + ": "+ entry.getValue());
 		// }
 
-		System.out.println("ORIGINAL FILE CONTENTS: \n"+ getContents(inputFile));
+		System.out.println("f_INPUT_FLOW_OBJECTS_FILE CONTENTS: \n"+ FileUtils.getContents(f_INPUT_FLOW_OBJECTS_FILE));
 
 		String gORj ;
 		String globalArrays;
 		try {
 //			gORj = (String) (json_not_jackson?getGson(listita):mapper.writeValueAsString(listita));
 //			setContents(outputPlain, gORj);
-			globalArrays = getGlobalArrays(parser);
+			globalArrays = generateGlobalArrays(parser);
 			
 //			setContents(gsonPlain, globalArrays);
 //			System.out.println("NEW FILE CONTENTS: \n" + getContents(gsonPlain));
 			
-			setContents(flowINSERTS, getFlowInserts(listita,asJsonListita));
-			System.out.println("NEW FILE CONTENTS: \n" + getContents(flowINSERTS));		
+			FileUtils.setContents(f_OUTPUT_FLOW_OBJECTS_INSERTS, generateFlowInserts(flowObjectsList,asJsonListita));
+			System.out.println("f_OUTPUT_FLOW_OBJECTS_INSERTS CONTENTS: \n" + FileUtils.getContents(f_OUTPUT_FLOW_OBJECTS_INSERTS));		
 			
-			setContents(dayINSERTS, getDayInserts());
-			System.out.println("NEW FILE CONTENTS: \n" + getContents(dayINSERTS));		
+			FileUtils.setContents(f_OUTPUT_DAY_INSERTS, generateDayInserts());
+			System.out.println("f_OUTPUT_DAY_INSERTS CONTENTS: \n" + FileUtils.getContents(f_OUTPUT_DAY_INSERTS));		
+
+			FileUtils.setContents(f_OUTPUT_FILE_CITAS, generateCitasInserts(citasObjectsList));
+			System.out.println("f_OUTPUT_DAY_INSERTS CONTENTS: \n" + FileUtils.getContents(f_OUTPUT_DAY_INSERTS));		
 			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -73,7 +85,69 @@ public class Puente {
 
 
 
-	private static String getGlobalArrays(FlowXMLParser parser) {
+	private static List<Cita> parseCitasInputFile(File f_INPUT_FILE_CITAS) {
+		
+		citasObjectsList 	= new ArrayList<Cita>();
+		Cita cita 			= null;
+		//StringTokenizer st = new StringTokenizer( "hello,this.is:a;test", ",.:;");
+		String[] arr		= null;
+		// ...checks on aFile are elided
+		try {
+			// use buffering, reading one line at a time
+			// FileReader always assumes default encoding is OK!
+			BufferedReader input = new BufferedReader(new FileReader(f_INPUT_FILE_CITAS));
+			try {
+				String line = null; // not declared within while loop
+				/*
+				 * readLine is a bit quirky : it returns the content of a line
+				 * MINUS the newline. it returns null only for the END of the
+				 * stream. it returns an empty String if two newlines appear in
+				 * a row.
+				 */
+				int couter = 0;
+				while ((line = input.readLine()) != null) {
+					
+					try{
+					if ( !line.trim().equals("")){
+						cita = new Cita();
+						arr = line.split("\t");
+						cita.setText(arr[1].trim());
+						cita.setAuthor(arr[2].trim());
+						
+						citasObjectsList.add(cita);
+					}
+					}catch (java.lang.ArrayIndexOutOfBoundsException ex){
+						System.out.println(couter);
+					}
+					couter++;
+					
+				}
+			} finally {
+				input.close();
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
+		return citasObjectsList;
+	}
+
+
+
+	private static String generateCitasInserts(List<Cita> citasObjectsList2) {
+		sb = new StringBuilder();
+		int counter = 0;
+		for (Cita it : citasObjectsList2){
+			sb.append("INSERT INTO " + DB_FLOW_TABLE + " (" + FLOW_KEY_ID + "," + FLOW_KEY_OBJECT + ") " +
+					 "VALUES (" + it.getAuthor()+ ", '" + it.getText()).append("');\n");
+			counter++;
+		}
+		return sb.toString();
+	}
+
+
+
+	private static String generateGlobalArrays(FlowXMLParser parser) {
 		String OneCodes = "";
 		String TwoCodes = "";
 		String OneDescriptions = "";
@@ -103,7 +177,7 @@ public class Puente {
 		return sb.toString();
 	}
 
-	private static String getFlowInserts(List<Stage> listita, List<String> listitaJSON) {
+	private static String generateFlowInserts(List<Stage> listita, List<String> listitaJSON) {
 		sb = new StringBuilder();
 		int counter = 0;
 		for (Stage it : listita){
@@ -114,7 +188,7 @@ public class Puente {
 		return sb.toString();
 	}
 
-	private static String getDayInserts() {
+	private static String generateDayInserts() {
 		sb = new StringBuilder();
 		sb.append("INSERT INTO " + DB_CIGARS_H_TABLE+ " (" + CIGARS_H_KEY_DAY + "," + CIGARS_H_KEY_COUNT+ ") " + "VALUES (1,17);\n");
 		sb.append("INSERT INTO " + DB_CIGARS_H_TABLE+ " (" + CIGARS_H_KEY_DAY + "," + CIGARS_H_KEY_COUNT+ ") " + "VALUES (2,23);\n");
@@ -124,7 +198,7 @@ public class Puente {
 	}
 	
 	private static List<String> getGson(List<Stage> listaInserts) {
-
+		asJsonListita = new ArrayList<String> ();
 		try {
 			for (Stage it : listaInserts){
 				asJsonListita.add(mapper.writeValueAsString(it));
@@ -142,82 +216,5 @@ public class Puente {
 		return asJsonListita;
 	}
 
-	/**
-	 * Fetch the entire contents of a text file, and return it in a String. This
-	 * style of implementation does not throw Exceptions to the caller.
-	 * 
-	 * @param aFile
-	 *            is a file which already exists and can be read.
-	 */
-	static public String getContents(File aFile) {
-		// ...checks on aFile are elided
-		StringBuilder contents = new StringBuilder();
 
-		try {
-			// use buffering, reading one line at a time
-			// FileReader always assumes default encoding is OK!
-			BufferedReader input = new BufferedReader(new FileReader(aFile));
-			try {
-				String line = null; // not declared within while loop
-				/*
-				 * readLine is a bit quirky : it returns the content of a line
-				 * MINUS the newline. it returns null only for the END of the
-				 * stream. it returns an empty String if two newlines appear in
-				 * a row.
-				 */
-				while ((line = input.readLine()) != null) {
-					contents.append(line);
-					contents.append(System.getProperty("line.separator"));
-				}
-			} finally {
-				input.close();
-			}
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		}
-
-		return contents.toString();
-	}
-
-	/**
-	 * Change the contents of text file in its entirety, overwriting any
-	 * existing text.
-	 * 
-	 * This style of implementation throws all exceptions to the caller.
-	 * 
-	 * @param aFile
-	 *            is an existing file which can be written to.
-	 * @throws IllegalArgumentException
-	 *             if param does not comply.
-	 * @throws FileNotFoundException
-	 *             if the file does not exist.
-	 * @throws IOException
-	 *             if problem encountered during write.
-	 */
-	static public void setContents(File aFile, String aContents)
-			throws FileNotFoundException, IOException {
-		if (aFile == null) {
-			throw new IllegalArgumentException("File should not be null.");
-		}
-		if (!aFile.exists()) {
-			throw new FileNotFoundException("File does not exist: " + aFile);
-		}
-		if (!aFile.isFile()) {
-			throw new IllegalArgumentException("Should not be a directory: "
-					+ aFile);
-		}
-		if (!aFile.canWrite()) {
-			throw new IllegalArgumentException("File cannot be written: "
-					+ aFile);
-		}
-
-		// use buffering
-		Writer output = new BufferedWriter(new FileWriter(aFile));
-		try {
-			// FileWriter always assumes default encoding is OK!
-			output.write(aContents);
-		} finally {
-			output.close();
-		}
-	}
 }
